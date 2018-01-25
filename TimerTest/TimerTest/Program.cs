2a5d20eb672d93cbb920cfa355fe10b9d982e214
HyundongHwang <hhd2002@gmail.com>
2018-01-25 14:46:16 +0900

    modify

---
+++ TimerTest/TimerTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main TID : {Thread.CurrentThread.ManagedThreadId}");
+
+            var timer = new Timer((arg) =>
+            {
+                Console.WriteLine($"timer callback TID : {Thread.CurrentThread.ManagedThreadId} start !!!");
+                Thread.Sleep(3000);
+                Console.WriteLine($"timer callback TID : {Thread.CurrentThread.ManagedThreadId} end !!!");
+            }, null, 0, 1000);
+
+            Console.ReadLine();
+        }
+    }
+}
